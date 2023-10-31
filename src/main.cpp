874f8a05e7d72165454080f70b0549d2b2256ce1
Xavier Côté-Daviau <112489173+xavcd@users.noreply.github.com>
2023-10-31 14:19:29 -0400

    Setup des composants de base

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+ESP8266WebServer httpd(80);
+double tempMijoteuse = 0.00;
+byte decimalAmount = 2;
+String htmlRoot = "";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Déclaration des méthodes
+void HandleRoot();
+void ReadTemp();
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Creation du AP...");
+  WiFi.softAP("xavcd", "Toto123!");
+
+  Serial.println(WiFi.softAPIP());
+
+  httpd.on("/", HandleRoot);
+  httpd.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+    ReadTemp();
+    httpd.handleClient(); // Appeler le plus souvent possible
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void HandleRoot()
+{
+    htmlRoot =  "<html>" +
+    htmlRoot +=     "<body>" +
+    htmlRoot +=         "Temperature: " + String(tempMijoteuse, decimalAmount) + 
+    htmlRoot +=     "</body>" +
+    htmlRoot += "</html>";
+  
+    httpd.send(200, "text/html", htmlRoot);
+}
+
+void ReadTemp() 
+{
+    int input = analogRead(A0);
+    double voltage = input * 5.0 / 1023.0;
+    double resistance = 10000.0 * voltage / (5.0 - voltage);
+    double tempKelvin = 1.0 / (1.0 / 298.15 + log(resistance / 10000.0) / 3977.0); // B (beta) = 3977
+    tempMijoteuse = tempKelvin - 273.15;
 }
